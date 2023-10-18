e025364d03d11b2939bdb2c1edaf81a798a2b10d
Agu Christopher <aguchris740@gmail.com>
2023-10-18 23:26:33 +0100

    Added fonts

--- app/_layout.js
+++ app/_layout.js
@@ -1,10 +1,15 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default () => {
+  let [fontsLoaded] = useFonts({
+    PBold: require("../assets/fonts/Poppins-Medium.ttf"),
+  });
   return (
     <Stack
       screenOptions={{
-        statusBarColor: "dodgerblue",
+        // statusBarColor: "#fff",
+        headerShown: false,
       }}
     >
       <Stack.Screen name="index" options={{ headerShown: false }} />
--- app/index.js
+++ app/index.js
@@ -1,18 +1,68 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 import { FlatList } from "react-native-gesture-handler";
 import { Image } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 const app = () => {
+  let { width, height } = Dimensions.get("screen");
   return (
     <View style={styles.container}>
-      <FlatList
-        data={["", "", "", ""]}
-        renderItem={({ item, index }) => {
-          <Image source={require(`../assets/males/m-${index}`)} />;
-        }}
-      />
+      <StatusBar style="dark" />
+      <View></View>
       <Text>app</Text>
+      <TouchableOpacity
+        style={{
+          height: 60,
+          backgroundColor: "black",
+          width: width * 0.9,
+          alignSelf: "center",
+          borderRadius: 10,
+          alignContent: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontFamily: "PBold",
+            alignSelf: "center",
+            fontSize: 18,
+          }}
+        >
+          Login
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          height: 60,
+          backgroundColor: "#fff",
+          width: width * 0.9,
+          alignSelf: "center",
+          borderRadius: 10,
+          alignContent: "center",
+          justifyContent: "center",
+          borderWidth: 2,
+          margin: 15,
+        }}
+      >
+        <Text
+          style={{
+            color: "#000",
+            fontFamily: "PBold",
+            alignSelf: "center",
+            fontSize: 18,
+          }}
+        >
+          Register
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -21,7 +71,7 @@ export default app;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "dodgerblue",
+    backgroundColor: "#fff",
     flex: 1,
   },
 });
