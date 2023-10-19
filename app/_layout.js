ca26ea1b1fbe815c3fa748c6c7ec3b205d1d53ed
Agu Christopher <aguchris740@gmail.com>
2023-10-19 12:41:35 +0100

    updates

--- app/_layout.js
+++ app/_layout.js
@@ -1,11 +1,18 @@
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
 
 export default () => {
   let [fontsLoaded] = useFonts({
     PBold: require("../assets/fonts/Poppins-Medium.ttf"),
     PBolder: require("../assets/fonts/Poppins-Bold.ttf"),
   });
+  if (fontsLoaded)
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <ActivityIndicator size={"large"} />
+      </View>
+    );
   return (
     <Stack
       screenOptions={{
